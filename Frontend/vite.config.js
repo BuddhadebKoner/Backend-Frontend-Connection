46feb8727f1bf54f8333ec864922a8fc9cce1b2f
BuddhadebKoner <jeetkoner36@gmail.com>
2024-06-26 18:03:17 +0530

    url chnages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://hindisong-backend.onrender.com',
+        target: 'https://hindisong-backend.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
